70ca8f4c0909eeb93a261675c2d03d3112ed779d
theshrish46 <theshrish46@gmail.com>
2023-02-11 23:20:35 +0530

    modified html file

--- script.js
+++ script.js
@@ -1,24 +1,10 @@
-var boom = new Audio('./sounds/ride.wav')
 
-const container = document.querySelector('.container');
+
 
 const keys = document.querySelectorAll('.key')
 
-const keyCode = {
-    A: 65,
-    S: 83,
-    D: 68,
-    F: 70,
-    G: 71,
-    H: 72,
-    J: 74,
-    K: 75,
-    L: 76,
-};
-
-keys.forEach((key) => {
-    key.addEventListener('click', function() {
-        // alert('hai')
-        boom.play()
+keys.forEach(key => {
+    key.addEventListener('click', (e) => {
+        
     })
-})
+});
