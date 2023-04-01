d8c2f181a7df1c36c9657fdc69347b8a8cfaf6e4
theshrish46 <theshrish46@gmail.com>
2023-04-01 14:23:40 +0530

    on 1-4-23

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
+const keys = document.querySelectorAll('kbd');
+const audio = document.querySelectorAll('audio')
 
-
-
-const keys = document.querySelectorAll('.key')
-
-keys.forEach(key => {
-    key.addEventListener('click', (e) => {
-        
+keys.forEach((key) => {
+    key.addEventListener('keypress', (e) => {
+        // e.preventDefault()
+        console.log(e.target);
     })
-});
+})
